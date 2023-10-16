ba2937d9755da2bd8edc95e3e501463f2362cb4a
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-10-16 10:07:39 +0100

    Create some Error handling to ensure correct Data is uploaded to the spreadsheet

--- run.py
+++ run.py
@@ -33,25 +33,55 @@ def main_menu():
 
 survey_questions = [
      "Question 1: what is your age?",
-     "Question 2: Do you think Instagram influencers are having a positive impact on young people's self-esteem? Please type one of the following - Strongly disagree, Disagree, Neutral, Agree, Strongly agree",
-     "Question 3: How much does Instagram or other social media affect what you purchase in the beauty industry? Please type one of the following - A lot, A little, A moderate amount, Not much at all, A significant amount",
-     "Question 4: How often do you wear makeup? Please type one of the following - Rarely, Daily, Occasionally, Weekly, Monthly",
-     "Question 5: Do you feel investing in skin care products is worth it? Please type one of the following - Strongly disagree, Disagree, Neutral, Agree, Strongly agree",
-     "Question 6: How would you rate your self-esteem and body image? Please type one of the following - Very low, Low, Neutral, High, Very High",
-     "Question 7: Do you subscribe to beauty boxes or services that provide you with new products regularly? Please type one of the following - Yes, No",
-     "Question 8: How much do you typically spend on beauty products each month? Please type one of the following - under £25, £25 - £50, £50 - £100, £100 - £200, £200 - £300, Over £300",
-     "Question 9: Do you have a step by step skincare routuine? Please type one of the following - Yes, No",
-     "Question 10: Do you feel more attaractive when you are wearing makeup? Please type one of the following - Yes, No"
+     "Question 2: Do you think Instagram influencers are having a positive impact on young people's self-esteem? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree",
+     "Question 3: How much does Instagram or other social media affect what you purchase in the beauty industry? Please type one of the following - a lot, a little, a moderate amount, not much at all, a significant amount",
+     "Question 4: How often do you wear makeup? Please type one of the following - rarely, daily, occasionally, weekly, monthly",
+     "Question 5: Do you feel investing in skin care products is worth it? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree",
+     "Question 6: How would you rate your self-esteem and body image? Please type one of the following - very low, low, neutral, high, very high",
+     "Question 7: Do you subscribe to beauty boxes or services that provide you with new products regularly? Please type one of the following - yes, no",
+     "Question 8: How much do you typically spend on beauty products each month? Please type one of the following - under £25, £25 - £50, £50 - £100, £100 - £200, £200 - £300, over £300",
+     "Question 9: Do you have a step by step skincare routuine? Please type one of the following - yes, no",
+     "Question 10: Do you feel more attaractive when you are wearing makeup? Please type one of the following - yes, no"
 ]
 
-worksheet = SHEET.get_worksheet(1)
+worksheet = SHEET.get_worksheet(0)
 
 def input_data():
-    responses = []
+    valid_responses = [
+        None,  # The first question (age) can be any integer
+        ["strongly disagree", "disagree", "neutral", "agree", "strongly agree"],  # Question 2
+        ["a lot", "a little", "a moderate amount", "not much at all", "a significant amount"],  # Question 3
+        ["rarely", "daily", "occasionally", "weekly", "monthly"],  # Question 4
+        ["strongly disagree", "disagree", "neutral", "agree", "strongly agree"],  # Question 5
+        ["very low", "low", "neutral", "high", "very high"],  # Question 6
+        ["yes", "no"],  # Question 7
+        ["under £25", "£25 - £50", "£50 - £100", "£100 - £200", "£200 - £300", "over £300"],  # Question 8
+        ["yes", "no"],  # Question 9
+        ["yes", "no"],  # Question 10
+    ]
 
-    for question in survey_questions:
-        response = input(f"{question}: ")  # Get the response from the user
-        responses.append(response)
+    responses = []
+   
+    for i, question in enumerate(survey_questions):
+        while True:
+            response = input(f"{question}: ").lower()
+
+            if i == 0:  # For the age question
+                if response.isdigit():
+                    responses.append(response)
+                    break
+                else:
+                    print("Invalid response. Please enter a valid age (an integer).")
+            else:
+                valid_options = valid_responses[i]
+                if response in valid_options:
+                    responses.append(response)
+                    break
+                else:
+                    print(f"Invalid response. Please choose one of the following options: {', '.join(valid_options)}")
+
+
+
 
     # Find the last row with data
     last_row = len(worksheet.col_values(1)) + 1
