9f86a02cb9d6e14eeec0bb015754426cd47cdf85
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-10-18 15:18:20 +0100

    Amend survey questions variable and view data function to ensure question options are clear and easy for the user to see/ chose from by adding 2 new lines between each question

--- run.py
+++ run.py
@@ -34,16 +34,16 @@ def main_menu():
 
 
 survey_questions = [
-     "1: what is your age?",
-     "2: Do you think Instagram influencers are having a positive impact on young people's self-esteem? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree",
-     "3: How much does Instagram or other social media affect what you purchase in the beauty industry? Please type one of the following - a lot, a little, a moderate amount, not much at all, a significant amount",
-     "4: How often do you wear makeup? Please type one of the following - rarely, daily, occasionally, weekly, monthly",
-     "5: Do you feel investing in skin care products is worth it? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree",
-     "6: How would you rate your self-esteem and body image? Please type one of the following - very low, low, neutral, high, very high",
-     "7: Do you subscribe to beauty boxes or services that provide you with new products regularly? Please type one of the following - yes, no",
-     "8: How much do you typically spend on beauty products each month? Please type one of the following - under £25, £25 - £50, £50 - £100, £100 - £200, £200 - £300, over £300",
-     "9: Do you have a step by step skincare routuine? Please type one of the following - yes, no",
-     "10: Do you feel more attractive when you are wearing makeup? Please type one of the following - yes, no"
+     "1: what is your age?\n",
+     "2: Do you think Instagram influencers are having a positive impact on young people's self-esteem? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree\n",
+     "3: How much does Instagram or other social media affect what you purchase in the beauty industry? Please type one of the following - a lot, a little, a moderate amount, not much at all, a significant amount\n",
+     "4: How often do you wear makeup? Please type one of the following - rarely, daily, occasionally, weekly, monthly\n",
+     "5: Do you feel investing in skin care products is worth it? Please type one of the following - strongly disagree, disagree, neutral, agree, strongly agree\n",
+     "6: How would you rate your self-esteem and body image? Please type one of the following - very low, low, neutral, high, very high\n",
+     "7: Do you subscribe to beauty boxes or services that provide you with new products regularly? Please type one of the following - yes, no\n",
+     "8: How much do you typically spend on beauty products each month? Please type one of the following - under £25, £25 - £50, £50 - £100, £100 - £200, £200 - £300, over £300\n",
+     "9: Do you have a step by step skincare routuine? Please type one of the following - yes, no\n",
+     "10: Do you feel more attractive when you are wearing makeup? Please type one of the following - yes, no\n"
 ]
 
 worksheet = SHEET.get_worksheet(0)
@@ -91,7 +91,7 @@ def input_data():
 
 def view_data_analysis():
     print("Data Analysis Menu")
-    print("Choose a question number to analyze (1-10):")
+    print("Choose a question number to analyze (1-10):\n" + "\n".join(survey_questions))
     question_number = int(input())
 
     # Ensure the chosen question number is valid
