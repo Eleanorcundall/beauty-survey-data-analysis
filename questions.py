0491997ac3d7da307f5789146c02e04cc09f5972
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-11-10 09:53:35 +0000

    Amend Line length to fit in with pep8 standards and to pass linter for questions.py file

--- questions.py
+++ questions.py
@@ -3,13 +3,13 @@ survey_questions = [
     "1: what is your age?\n",
     """2: Do you think Instagram influencers are having a positive impact on
 young people's self-esteem?""",
-    """3: How much does Instagram or other social media affect what you purchase
-in the beauty industry?""",
+    """3: How much does Instagram or other social media affect what you
+purchase in the beauty industry?""",
     """4: How often do you wear makeup?""",
     """5: Do you feel investing in skin care products is worth it?""",
     """6: How would you rate your self-esteem and body image?""",
-    """7: Do you subscribe to beauty boxes or services that provide you with new
-products regularly?""",
+    """7: Do you subscribe to beauty boxes or services that provide you with
+new products regularly?""",
     """8: How much do you typically spend on beauty products each month?""",
     """9: Do you have a step by step skincare routine?""",
     """10: Do you feel more attractive when you are wearing makeup?""",
@@ -26,8 +26,8 @@ disagree
 neutral
 agree
 strongly agree""",
-    """3: How much does Instagram or other social media affect what you purchase
-in the beauty industry? Please type one of the following -
+    """3: How much does Instagram or other social media affect what you
+purchase in the beauty industry? Please type one of the following -
 a lot
 a little
 a moderate amount
@@ -41,10 +41,10 @@ occasionally
 weekly
 monthly\n""",
     """5: Do you feel investing in skin care products is worth it?
-Please type one of the following - 
+Please type one of the following -
 strongly disagree
 disagree
-neutral 
+neutral
 agree
 strongly agree""",
     """6: How would you rate your self-esteem and body image?
@@ -54,12 +54,12 @@ low
 neutral
 high
 very high""",
-    """7: Do you subscribe to beauty boxes or services that provide you with new
-products regularly? Please type one of the following -
+    """7: Do you subscribe to beauty boxes or services that provide you with
+new products regularly? Please type one of the following -
 yes
 no""",
     """8: How much do you typically spend on beauty products each month?
-Please type one of the following - 
+Please type one of the following -
 under £25
 £25 - £50
 £50 - £100
@@ -67,11 +67,11 @@ under £25
 £200 - £300
 over £300""",
     """9: Do you have a step by step skincare routine?
-Please type one of the following - 
+Please type one of the following -
 yes
 no""",
     """10: Do you feel more attractive when you are wearing makeup?
-Please type one of the following - 
+Please type one of the following -
 yes
 no""",
 ]
